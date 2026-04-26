cea6b12ed53c795477a255282c3f51d500cca760
MiaoSuperCool <19808373423@139.com>
2026-04-26 11:09:06 +0800

    第二版文件管理器，将测试文件夹中对应的文件指定不同的文件夹存入

--- file_organizer.py
+++ file_organizer.py
@@ -9,15 +9,21 @@ categories={
   "Archives":{".zip",".tar",".gz"},
 }
 
+dir_path={
+    "Images":Path("C:/Users/liu'miao/Pictures"),
+    "Documents":Path("D:/Miao/Documents"),
+    "Archives":Path("D:/Miao/Archives"),
+}
+
 for item in source_file.iterdir():
     if item.is_file() and not item.name.startswith("."):
         ext=item.suffix.lower()
         for folder_name,ext_set in categories.items():
             if ext in ext_set:
-                dest_folder=source_file / folder_name
+                dest_folder=dir_path[folder_name]/ folder_name
                 dest_folder.mkdir(exist_ok=True)
                 shutil.move(str(item),str(dest_folder / item.name))
-                print(f"already move:{item.name} to {folder_name}/")
+                print(f"already move:{item.name} to {dest_folder}/")
                 break
 
 
